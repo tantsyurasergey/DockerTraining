eeaa78cdeaa4c5d61551cee4e17e5b54f097b58a
Sergey Tantsyura <sergey.tantsyura@sitecore.com>
2019-10-15 23:02:25 +0300

    Change sequence of commands in cleanup script

--- src/05_Orchestrating_Systems_with_Docker/AdvancedWebApp_Orchestration/00_Cleanup.ps1
+++ src/05_Orchestrating_Systems_with_Docker/AdvancedWebApp_Orchestration/00_Cleanup.ps1
@@ -1,5 +1,5 @@
-docker container stop docker_training_advancedwebapp
-docker container stop docker_training_prometheus
 docker container stop docker_training_grafana
+docker container stop docker_training_prometheus
+docker container stop docker_training_advancedwebapp
 docker network rm docker_training_network_advancedwebapp
 docker system prune
