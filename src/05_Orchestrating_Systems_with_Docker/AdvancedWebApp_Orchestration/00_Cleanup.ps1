54636ddb06bff8e1dc5e12fce73623b75604104b
Sergey Tantsyura <sergey.tantsyura@sitecore.com>
2019-10-16 20:27:03 +0300

    Fix cleanup script used in Docker Compose demo

--- src/05_Orchestrating_Systems_with_Docker/AdvancedWebApp_Orchestration/00_Cleanup.ps1
+++ src/05_Orchestrating_Systems_with_Docker/AdvancedWebApp_Orchestration/00_Cleanup.ps1
@@ -2,5 +2,9 @@ docker container stop docker_training_grafana
 docker container stop docker_training_prometheus
 docker container stop docker_training_advancedwebapp
 docker container stop docker_training_sqlserver
+
 docker network rm docker_training_network_advancedwebapp
+
 docker system prune
+
+docker volume rm $(docker volume ls -qf dangling=true)
